667ef7d08ddf68a5dd5375a7ad068caff39e6f98
Rae <akumakransdor@gmail.com>
2026-06-10 10:16:33 +0100

    添加前端项目框架

---
+++ CampusConnect/CampusConnect/campus-connect-frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+// 页面主挂载入口
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+    'Microsoft YaHei', 'Arial', sans-serif;
+  background-color: #f5f7fa;
+  color: #303133;
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
