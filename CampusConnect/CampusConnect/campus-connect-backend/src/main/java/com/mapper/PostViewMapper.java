8d5703c9d52595927d9a2b0dc39da5a8cc43b753
1122333411 <2013887248@qq.com>
2026-06-22 20:10:31 +0800

    Create PostViewMapper.java
    新增PostViewMapper，帖子浏览记录数据操作

---
+++ CampusConnect/CampusConnect/campus-connect-backend/src/main/java/com/mapper/PostViewMapper.java
@@ -0,0 +1,9 @@
+package com.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.entity.PostView;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PostViewMapper extends BaseMapper<PostView> {
+}
